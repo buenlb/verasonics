35286b3bcf045613498bf6d0e2209d194da17c8b
buenlb <tdwebb@stanford.edu>
2019-02-12 16:54:06 -0700

    Added functionality to deal with adding paths on any Windows system.

--- src/lib/computeTXDelays_monkey.m
+++ src/lib/computeTXDelays_monkey.m
@@ -2,14 +2,28 @@
 % 
 % @INPUTS
 %   focus: the desired focal location. Must be referenced to the same
-%      origin as the element locations in Trans
+%      origin as the element locations in Trans. A 3x1 vector <x,y,z> (mm).
 %   Trans: Struct defining the transducer. This is the same struct expected
 %      by VSX
+%   Resource: VSX Resource struct. At a minimum must include
+%     Resource.Parameters.speedOfSound
 % 
 % @OUTPUTS
 %   delays: Delays to apply to each element in order to focus at the
 %     desired target
 
-function computeTXDelays_monkey(focus,Trans)
+function delays = computeTXDelays_monkey(focus,Trans,Resource)
 
-elLocations = Trans.ElementPos;
+elLocations = Trans.ElementPos(:,1:3);
+f = Trans.frequency;
+c = Resource.Parameters.speedOfSound;
+
+% Vectors pointing from the elements to the focus
+R = elLocations-repmat(focus,Trans.numelements,1);
+
+% For a spherical focusing law the delays are simply set to make each
+% element arrive at the same time
+delays = sqrt(sum(R.^2,2))/c;
+
+% Convert delays to fractions of a period.
+delays = delays*f;
--- src/setupScripts/setupTransmit.m
+++ src/setupScripts/setupTransmit.m
@@ -1,7 +1,24 @@
 clear; close all; clc;
 
+%% Setup paths
+% Find the directory this script is in (could be different since this can
+% be run in simulation on individual laptops as well as on the main
+% machine) and add the lib path. This will also be used to save the
+% resulting mat file.
+workingDirectory = which('setupTransmit.m');
+idx = strfind(workingDirectory,'\');
+workingDirectory = workingDirectory(1:idx(end));
+srcDirectory = workingDirectory(1:idx(end-1));
+
+if ~exist('computeTXDelays_monkey.m','file')
+    addpath([srcDirectory,'lib'])
+    addpath([srcDirectory,'MATFILES'])
+end
+
+%% Setup the transducer
 setupVerasonicsStruct;
 
+%% Verasonics Resource Struct
 Resource.Parameters.numTransmit = 256;      % number of transmit channels.
 Resource.Parameters.numRcvChannels = 256;    % number of receive channels.
 Resource.Parameters.speedOfSound = 1540;    % set speed of sound in m/sec before calling computeTrans
@@ -9,12 +26,17 @@ Resource.Parameters.verbose = 2;
 Resource.Parameters.initializeOnly = 1;
 Resource.Parameters.simulateMode = 1;
 
+%% Setup the transmitted waveform
 TW(1).type = 'parametric';
 waveform = [Trans.frequency,0.667,2,1];
 TW(1).Parameters = waveform;
 
+%% Create the Transmit event
 TX(1).waveform = 1;
 TX(1).Apod = ones(1,Trans.numelements);
+
+%% I have had trouble getting the default Verasonics function to work so this is ignored - I left it here in case it is useful in the future.
+if 0
 % This is the origin from which you want to steer. Generally this will be
 % just the origin of the Tx but if you want to do something fancy (ex: wide
 % beam imaging with the focus behind the Tx) setting it to something
@@ -23,22 +45,17 @@ TX(1).Origin = [0,0,0];
 TX(1).focus = r/lambda; % The radius of the transducer. r is defined in setupVerasonicsStruct
 TX(1).steer = [0*pi/180,0];
 TX(1).Delay = computeTXDelays(TX);
+end
 
-TX(2).waveform = 1;
-TX(2).Apod = ones(1,Trans.numelements);
-% This is the origin from which you want to steer. Generally this will be
-% just the origin of the Tx but if you want to do something fancy (ex: wide
-% beam imaging with the focus behind the Tx) setting it to something
-% non-zero may be useful.
-TX(2).Origin = [0,0,0]; 
-TX(2).focus = r/lambda; % The radius of the transducer. r is defined in setupVerasonicsStruct
-TX(2).steer = [0*pi/180,25*pi/180];
-TX(2).Delay = computeTXDelays(TX(2));
+%% Compute delays with custom function
+TX(1).Delay = computeTXDelays_monkey([5,r,0],Trans,Resource);
 
+%% Set up PData in order to visualize transmit
 PData.PDelta = ones(1,3);
 PData.Size = ceil(2*r/lambda)*ones(1,3);
 PData.Origin = [-PData.Size(1)/2,PData.Size(2)/2,0];
 
+%% Receive structure
 % Specify Receive structure array -
 Receive(1).Apod = ones(1,128); %V64, ones(1,64); V64LE [ones(1,64) zeros(1,64)];
 Receive(1).startDepth = 0;
@@ -52,6 +69,7 @@ Receive(1).sampleMode = 'NS200BW';
 Receive(1).LowPassCoef = [];
 Receive(1).InputFilter = [];
 
+%% Events
 % Specify sequence events.
 Event(1).info = 'Acquire RF Data.';
 Event(1).tx = 1; % use 1st TX structure.
@@ -61,14 +79,8 @@ Event(1).process = 0; % no processing
 Event(1).seqControl = 1; % transfer data to host
 SeqControl(1).command = 'transferToHost';
 
-Event(2).info = 'Acquire RF Data.';
-Event(2).tx = 2; % use 1st TX structure.
-Event(2).rcv = 1; % use 1st Rcv structure.
-Event(2).recon = 0; % no reconstruction.
-Event(2).process = 0; % no processing
-Event(2).seqControl = 1; % transfer data to host
-SeqControl(1).command = 'transferToHost';
 
-save C:\Users\Taylor\Documents\Projects\MonkeyTx\setupTransmit.mat
+%% Output resulting MAT file that will be passed to VSX
+save([srcDirectory,'MATFILES/setupTransmit.mat'])
 
 % showTXPD
